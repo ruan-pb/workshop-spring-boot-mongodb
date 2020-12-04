9da290ffeb35a71f7a6c18bef637a511615f735f
Ruan-_pb <ruancruzsoares@hotmail.com>
2020-12-04 11:16:34 -0300

    Created Project

---
+++ src/main/java/WorkShopMongoDB/WorkShopMongoDbApplication.java
@@ -0,0 +1,13 @@
+package WorkShopMongoDB;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkShopMongoDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkShopMongoDbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/WorkShopMongoDB/WorkShopMongoDbApplicationTests.java
@@ -0,0 +1,13 @@
+package WorkShopMongoDB;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkShopMongoDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
